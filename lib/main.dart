958a659fe2a15d997369ce95582f344c1c3ef1dd
hoit1302 <jueun000101@gmail.com>
2021-02-02 15:06:10 +0900

    feat(main.dart): add firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'login_page.dart';
 import 'my_page.dart';
 import 'input_page.dart';
 import 'chart_page.dart';
 
-void main() => runApp(MainPage());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MainPage());
+}
 
 class MainPage extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
